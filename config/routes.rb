b1f65640c399730df4e2cbec66973c4da47314c3
KOHEI FUKUSHIMA <pecpec1129@gmail.com>
2022-05-17 10:23:22 +0000

    [Finish]4-19

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   get '/homes/about' => 'homes#about',as:"about"
  devise_for :users
  resources :post_images,only: [:new,:create,:index,:show,:destroy] do
-   resources :post_comments, only:[:create,:destroy]
-   
- end  
+   resource :favorites,only: [:create, :destroy]
+
+   resources :post_comments,only:[:create,:destroy]
+ end
  resources :users,only: [:show,:edit,:update]
 end
 
